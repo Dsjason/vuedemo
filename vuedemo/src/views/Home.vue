622be34339c79692342d5e5555dc069f65ebd6eb
dsjason <zhrong819@163.com>
2020-06-11 23:03:02 +0800

    vuedemo提交

---
+++ vuedemo/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">{{count}}</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Home',
+  computed: {
+    ...mapState(['count'])
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
+    }
+  }
+}
+</script>
